8ad500dae58fc50b52965231b01fbd6a5a98d44a
ORCT <sonic407@naver.com>
2021-11-03 00:42:57 +0900

    new

---
+++ CodeUp_6082.py
@@ -0,0 +1,6 @@
+n = int(input())
+for i in range(1,n+1):
+    if str(i).find('3') != -1 or str(i).find('6') != -1 or str(i).find('9') != -1:
+        print('X', end = ' ')
+    else:
+        print(i, end = ' ')
