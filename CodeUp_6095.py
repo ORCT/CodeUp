8adac609d8b95bce88347fe9f3c22195f931bcbd
ORCT <sonic407@naver.com>
2021-11-17 15:38:42 +0900

    list comprehension

---
+++ CodeUp_6095.py
@@ -0,0 +1,9 @@
+board = [[0 for i in range(20)]for j in range(20)]
+n = int(input())
+for i in range(n):
+    r,c = map(int, input().split())
+    board[r][c] = 1
+for i in range(1,20):
+    for j in range(1,20):
+        print(board[i][j], end = ' ')
+    print()
