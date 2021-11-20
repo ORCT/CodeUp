2793ca85456dbc774594f0d88ca942901dca9e24
ORCT <sonic407@naver.com>
2021-11-20 20:46:30 +0900

    counting sort

--- CodeUp_3004.py
+++ CodeUp_3004.py
@@ -1,18 +1,11 @@
-#n = int(input())
-#num_list = list(map(int, input().split()))
-#sorted_num_list = sorted(num_list)
-#number_sequence = []
-#for i in range(n):
-#    for j in range(n):
-#        if sorted_num_list[j] == num_list[i]:
-#            number_sequence.append(j)
-#for i in range(n):
-#    print(number_sequence[i], end = ' ')
 n = int(input())
+cnt = [0 for i in range(500001)]
 num_list = list(map(int, input().split()))
-cnt_list = [0 for i in range(500001)]
+for i in range(n):
+    num = num_list[i]
+    cnt[num] += 1
+for i in range(len(cnt)-1):
+    cnt[i+1] += cnt[i]
 for i in num_list:
-    cnt_list[i] += 1
-for i in range(len(cnt_list)):
-    for j in range(cnt_list[i]):
-        print(i)
+    print(cnt[i]-1, end = ' ')
+#O(n)
